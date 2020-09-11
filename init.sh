ae75ede2b0ad514452b2f6b40ea7786239aba479
wangyaxian <wangyaxian@higohappy.com>
2020-09-11 16:27:27 +0800

    chore: 构建

---
+++ init.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# 创建项目目录
+mkdir -p /data/www
+
+# 克隆项目
+cd /data/www && git clone https://github.com/duiying/backend-access
+cd /data/www && git clone https://github.com/duiying/passport-service
+
+
+
+
+
+
+
+
