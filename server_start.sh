31ffc88c9dc97238a1af3522f3f1e7caebd96ba9
wangyaxian <wangyaxian@higohappy.com>
2020-09-14 16:53:41 +0800

    feat: 启动脚本

--- server_start.sh
+++ server_start.sh
@@ -1,4 +1,11 @@
 #!/usr/bin/env bash
 
-docker exec -it backend-access bash -c 'composer update && php bin/hyperf.php start'
-docker exec -it passport-service bash -c 'composer update && php bin/hyperf.php start'
+echo "========================= backend-access 启动 begin =======================+======================================"
+docker exec -i backend-access bash -c 'composer update'
+docker exec -d backend-access bash -c 'php bin/hyperf.php start'
+echo "========================= backend-access 启动 end =====================+=========================================="
+
+echo "========================= passport-service 启动 begin ==============+============================================="
+docker exec -i passport-service bash -c 'composer update'
+docker exec -d passport-service bash -c 'php bin/hyperf.php start'
+echo "========================= passport-service 启动 end ===================+=========================================="
