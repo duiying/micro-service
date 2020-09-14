3cea0a7656f781f2b9c620785fe9bff6ae8f8fd3
shenjie <shenjie@higohappy.com>
2020-09-14 11:30:38 +0800

    MOD:增加鲁棒性

--- init.sh
+++ init.sh
@@ -1,12 +1,27 @@
 #!/usr/bin/env bash
 
 # 创建目录
-mkdir www
+if [ ! -d www ]; then
+	mkdir www
+fi
 
 # 进入目录
 cd www
 
 # 下载项目
-git clone https://github.com/duiying/backend-access && composer install
-git clone https://github.com/duiying/passport-service && composer install
+if [ -d backend-access ]; then
+	cd backend-access
+	git pull origin master
+	composer install
+else
+	git clone https://github.com/duiying/backend-access && cd backend-access && composer install
+fi
+
+if [ -d passport-service ]; then
+	cd passport-service
+        git pull origin master
+        composer install
+else
+	git clone https://github.com/duiying/passport-service && cd passport-service && composer install
+fi
 
