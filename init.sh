e0aa48a3572dcca3d482e83e7fe3c4eed9f92074
shenjie <shenjie@higohappy.com>
2020-09-14 11:13:09 +0800

    MOD: 修改目录结构

--- init.sh
+++ init.sh
@@ -1,16 +1,12 @@
 #!/usr/bin/env bash
 
-# 创建项目目录
-mkdir -p /data/www
+# 创建目录
+mkdir www
+
+# 进入目录
+cd www
 
 # 下载项目
-cd /data/www && git clone https://github.com/duiying/backend-access && composer install
-cd /data/www && git clone https://github.com/duiying/passport-service && composer install
-
-
-
-
-
-
-
+git clone https://github.com/duiying/backend-access && composer install
+git clone https://github.com/duiying/passport-service && composer install
 
