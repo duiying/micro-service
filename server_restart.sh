94d9e94d65246642a935d947a3fa88668b60efd0
wangyaxian <wangyaxian@higohappy.com>
2020-09-15 12:25:19 +0800

    chore: 重启脚本

--- server_restart.sh
+++ server_restart.sh
@@ -3,5 +3,5 @@
 if [ $# -eq 0 ];then
 	echo "必须提供参数 : backend-access | passport-service "
 else
-  docker restart $1
+  docker exec -d $1 bash -c 'ps aux | grep 'php' |grep -v grep |awk '{print $1}' | xargs kill -TERM && php bin/hyperf.php start'
 fi
